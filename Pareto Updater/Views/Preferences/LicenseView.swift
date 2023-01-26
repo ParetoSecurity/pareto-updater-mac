599dcb15b918a92b4baa2ad019f0b99323d1a973
Janez Troha <dz0ny@ubuntu.si>
2023-01-26 09:54:47 +0100

    Try to license via clipboard

--- Pareto Updater/Views/Preferences/LicenseView.swift
+++ Pareto Updater/Views/Preferences/LicenseView.swift
@@ -13,6 +13,16 @@ struct LicenseView: View {
     @Default(.license) var license
     @Default(.showBeta) var showBeta
 
+    func tryLicense() {
+        if let lic = NSPasteboard.general.pasteboardItems?.first?.string(forType: .string) {
+            if lic.contains("paretoupdater://"), !Constants.Licensed {
+                if let url = URL(string: lic) {
+                    AppDelegate().processAction(url)
+                }
+            }
+        }
+    }
+
     var body: some View {
         if Constants.Licensed {
             if !Defaults[.teamID].isEmpty {
@@ -33,7 +43,7 @@ struct LicenseView: View {
                 Text("You are running the free version of the app. Please consider purchasing the Personal lifetime license for unlimited devices!")
                 Link("Learn more »",
                      destination: URL(string: "https://paretosecurity.com/updater?utm_source=\(Constants.utmSource)&utm_medium=license-link")!)
-            }.frame(width: 350, height: 80).padding(25)
+            }.frame(width: 350, height: 80).padding(25).onAppear(perform: tryLicense)
         }
     }
 }
